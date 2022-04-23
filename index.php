974af4100856125f2f44cf97c8f1cac4589f9924
Matt <matt@spectrebot.net>
2022-04-23 14:49:12 +0100

    add bootstrap css

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"> 
     </head>
     <body>
         
