50c1e26cd03b0f0a21fb83f21de76c4ebb5e20a3
Matt <matt@spectrebot.net>
2022-04-23 16:43:03 +0100

    initialise home page

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Required Meta -->
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Bootstrap -->
-            <!-- Latest compiled and minified CSS -->
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"> 
-            <!-- Latest compiled JavaScript -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <!-- Custom CSS -->
+            <link rel="stylesheet" href="./style.css">
+        <!-- Favicons -->
+            <link rel="apple-touch-icon" sizes="180x180" SameSite="Strict" href="./asset/favicon/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" SameSite="Strict" href="./asset/favicon/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" SameSite="Strict" href="./asset/favicon/favicon-16x16.png">
+            <link rel="manifest" href="./site.webmanifest">
+        <!-- Open Graph tags -->
+            <meta property="og:title" content="CashHook | Get More Donations!"/>
+            <meta property="og:description" content="CashHook is a simple to use and swift setup webhook tool for Ko-Fi, BuyMeACoffee and Patreon!"/>
+            <meta property="og:image" SameSite="Strict" content="https://CashHook.tech/asset/og/mainoog.png"/>
+            <meta property="og:site_name" content="CashHook"/>
+            <meta name="og:url" SameSite="Strict" content="https://CashHook.tech/">
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:image" SameSite="Strict" content="https://CashHook.tech/asset/og/mainog.png" />
+            <meta property="twitter:site" content="CashHook" />
+            <meta name="theme-color" content="#3ec300">
+        <!-- SEO meta tags -->
+            <title>CashHook | Increase your rate of donations by giving your community the recognition they deserve!</title>
+            <meta name="description" content="CashHook is a simple to use and swift setup webhook tool for Ko-Fi, BuyMeACoffee and Patreon!">
+            <meta name="keywords" content="Ko-Fi, BuyMeACoffee, Patreon, Donation, Donate">
+            <meta name="author" content="Matt Davison">
     </head>
     <body>
-        
+        <!-- Navbar -->
+        <?php include ('./php/navbar.php');?>
     </body>
 </html>
