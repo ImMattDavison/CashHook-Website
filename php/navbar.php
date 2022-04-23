49cfadaa89711e51ec24ba7657837045787b482a
Matt <matt@spectrebot.net>
2022-04-23 16:49:46 +0100

    create navbar

---
+++ php/navbar.php
@@ -0,0 +1,47 @@
+<div class="justify-content-center bg-cashhook-green d-none d-sm-none d-md-block d-lg-block d-xl-block noSidePadding sticky-top">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-nav justify-content-center d-none d-sm-none d-md-block d-lg-block d-xl-block nav-padding align-items-center">
+    <div class="container align-items-center">
+        <a class="navbar-brand" href="../">
+            <img src="../asset/brand/logo.svg" alt="CashHook" height="50px"/>
+        </a>
+        <ul class="navbar-nav d-flex justify-content-end logo-nav">
+          <li class="nav-item">
+              <a class="nav-link active" href="../">Ko-Fi</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link active" href="../about">BuyMeACoffee</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link active" href="../portfolio">Patreon</a>
+          </li>
+        </ul>
+      </nav>
+</div>
+</div>
+<div class="noSidePadding bg-cashhook-green d-block d-sm-block d-md-none d-lg-none d-xl-none sticky-top">
+    <nav class="navbar navbar-expand-sm bg-nav navbar-light nav-padding">
+          <a class="navbar-brand" href="../">
+              <img src="../asset/brand/logo.svg" alt="CashHook" height="50px"/>
+          </a>
+                <button class="navbar-toggle justify-self-end" type="button" onclick="openNav()">
+                  &#9776;
+                </button>
+                <script>
+                  function openNav() {
+                    document.getElementById("myNav").style.width = "100%";
+                  }
+                  
+                  function closeNav() {
+                    document.getElementById("myNav").style.width = "0%";
+                  }
+                  </script>
+                  <div id="myNav" class="overlay">
+          <a href="#" class="closebtn" onclick="closeNav()">&times;</a>
+          <div class="overlay-content">
+            <a href="../">Ko-Fi</a>
+            <a href="../about">BuyMeACoffee</a>
+            <a href="../portfolio">Patreon</a>
+          </div>
+        </div>
+    </nav>
+</div>
