8d8602af59210abdc5c83a33a5d6efe8d302dee8
Matt <matt@spectrebot.net>
2022-04-23 19:02:32 +0100

    set footer content

---
+++ php/footer.php
@@ -0,0 +1,38 @@
+<footer class="container-fluid py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md">
+            <a href="../"><img src="./asset/brand/logo.svg" alt="footer logo" height="50px"/></a>
+          <small class="d-block mt-3 mb-3">© 2022 Matt Davison</small>
+        </div>
+        <div class="col-12 col-md first-footer-group">
+          <h4>Integrations</h4>
+          <ul class="list-unstyled text-small">
+            <li class="footer-link-item"><a class="footer-link" href="./#ko-fi">Ko-Fi</a></li>
+            <li class="footer-link-item"><a class="footer-link" href="./#buymeacoffee">BuyMeACoffee</a></li>
+            <li class="footer-link-item"><a class="footer-link" href="./#patreon">Patreon</a></li>
+          </ul>
+        </div>
+        <div class="col-12 col-md">
+          <h4>Donate</h4>
+          <ul class="list-unstyled text-small">
+            <li class="footer-link-item"><a class="footer-link" href="https://ko-fi.com/CashHook">Ko-Fi</a></li>
+            <li class="footer-link-item"><a class="footer-link" href="https://buymeacoffee.com/CashHook">BuyMeACoffee</a></li>
+          </ul>
+        </div>
+        <div class="col-12 col-md">
+          <h4>Developers</h4>
+          <ul class="list-unstyled text-small">
+            <li class="footer-link-item"><a class="footer-link" href="../privacy">Privacy Policy</a></li>
+            <li class="footer-link-item"><a class="footer-link" href="mailto:privacy@mjdws.com">Privacy Concerns</a></li>
+          </ul>
+        </div>
+        <div class="col-12 col-md">
+          <h4>Contact</h4>
+          <ul class="list-unstyled text-small">
+            <li class="footer-link-item"><a class="footer-link" href="mailto:info@mjdws.com">matt@mjdws.com</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</footer>
