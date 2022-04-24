8a036f90e42d0e9f275562e6f0763053074edce6
Matt <matt@spectrebot.net>
2022-04-24 14:28:52 +0100

    update id positioning

--- index.php
+++ index.php
@@ -54,8 +54,8 @@
                         <img class="example-image" src="https://images.unsplash.com/photo-1580519542036-c47de6196ba5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW9uZXl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="money image">
                     </div>
             </content>
-            <hr>
-            <h3 class="text-center integration-title" id="ko-fi">Ko-Fi Integration</h3><br>
+            <hr id="ko-fi">
+            <h3 class="text-center integration-title">Ko-Fi Integration</h3><br>
             <content class="d-flex flex-wrap align-items-center justify-content-center flexbox-mobile mb-10 mt-10">
                     <div class="half-col">
                         <img class="example-image" src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZG9uYXRlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="money">
@@ -69,8 +69,8 @@
                 <a class="btn btn-cashhook text-center" href="https://autocode.com/app/cashhook/cashhook-kofi/">Get ko-fi integration on Autocode.com</a>
                 <a class="btn btn-cashhook text-center" href="https://github.com/immattdavison/CashHook-KoFi">Get ko-fi integration on Github.com</a>
             </div>
-            <hr>
-            <h3 class="text-center integration-title" id="buymeacoffee">Buy Me A Coffee Integration</h3><br>
+            <hr id="buymeacoffee">
+            <h3 class="text-center integration-title">Buy Me A Coffee Integration</h3><br>
             <content class="d-flex flex-wrap align-items-center justify-content-center flexbox-mobile mb-10 mt-10">
                     <div class="half-col">
                         <img class="example-image" src="https://images.unsplash.com/photo-1541167760496-1628856ab772?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29mZmVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="money">
@@ -84,8 +84,8 @@
                 <a class="btn btn-cashhook text-center" href="https://autocode.com/app/cashhook/cashhook-bmc/">Get BMC integration on Autocode.com</a>
                 <a class="btn btn-cashhook text-center" href="https://github.com/immattdavison/CashHook-BMC">Get BMC integration on Github.com</a>
             </div>
-            <hr>
-            <h3 class="text-center integration-title" id="patron">Patreon Integration</h3><br>
+            <hr id="patreon">
+            <h3 class="text-center integration-title">Patreon Integration</h3><br>
             <content class="d-flex flex-wrap align-items-center justify-content-center flexbox-mobile mb-10 mt-10">
                     <div class="half-col">
                         <img class="example-image" src="https://images.unsplash.com/photo-1512358958014-b651a7ee1773?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGRvbmF0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="money">
